8c78eb4263a694beefbcf19b219c9bb239a09712
ThimuthEXE <thimuthexe@gmail.com>
2025-02-13 00:00:23 +0530

    C++ commit

---
+++ ascii 365 (+1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string paragraph;
+    int ascii,code;
+    char char1;
+
+    cout << "Enter the AK code : ";
+    cin >> code;
+
+    cout << "Enter a paragraph:\n";
+    cin.ignore();
+    getline(cin, paragraph);
+
+    size_t i = 0;
+    while (i < paragraph.length()) {
+        char currentChar = paragraph[i];
+        ascii = static_cast<int>(currentChar);
+        ascii -= code;
+        char1 = static_cast<char>(ascii);
+        cout << char1;
+        i++;
+    }
+
+    return 0;
+}
